8c0f4d1d122e936670b5bc152fd762fb0a9bcd95
Ансар Орхан Айдын <Ansa_ly26@itstep.edu.az>
2019-06-03 11:53:10 +0400

    sockets

--- Sorting/SortingServer/src/com/company/ChatServer.java
+++ Sorting/SortingServer/src/com/company/ChatServer.java
@@ -1,10 +1,75 @@
 package com.company;
 
-public class ChatServer {
-    public String ipAddress;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
 
-    public ChatServer(String address) {
-        this.ipAddress = address;
+
+public class ChatServer {
+    public int port;
+    public LinkedList<MyServer> servers = new LinkedList<>();
+    public ServerSocket server;
+
+    public ChatServer(int port) {
+        this.port = port;
     }
 
+    public void run() throws IOException {
+        this.server = new ServerSocket(port);
+        try {
+            while (true) {
+                Socket socket = server.accept();
+                try {
+                    this.servers.add(new MyServer(socket));
+                } catch (IOException e) {
+                    socket.close();
+                }
+            }
+        } finally {
+            server.close();
+        }
+    }
+
+//****************************Server*Class********************************************
+
+    class MyServer extends Thread {
+        public Socket socket;
+        public BufferedReader reader;
+        public BufferedWriter writer;
+
+        public MyServer(Socket socket) throws IOException {
+            this.socket = socket;
+            this.reader =
+                    new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.writer =
+                    new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            start();
+        }
+
+        @Override
+        public void run() {
+            String word;
+            try {
+                while (true) {
+                    word = reader.readLine();
+                    if (word.equals("stop")) {
+                        break;
+                    }
+                    for (MyServer vr : servers) {
+                        if (vr.socket != this.socket) {
+                            vr.send(word);
+                        }
+                    }
+                }
+            } catch (IOException e) { }
+        }
+
+        private void send(String msg) {
+            try {
+                writer.write(msg + "\n");
+                writer.flush();
+            } catch (IOException ignored) { }
+        }
+    }
 }
--- Sorting/SortingServer/src/com/company/Main.java
+++ Sorting/SortingServer/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
+    public static final int PORT = 27001;
 
     public static void main(String[] args) {
-
+        ChatServer server = new ChatServer(PORT);
+        try {
+            server.run();
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
     }
 }
